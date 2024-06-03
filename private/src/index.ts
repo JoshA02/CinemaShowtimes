9b1114ad1a161fc9008637689a6e0e0b44e5d484
Josh <joshuav2012@live.co.uk>
2024-06-03 23:20:57 +0100

    Implemented basic caching

--- private/src/index.ts
+++ private/src/index.ts
@@ -1,15 +1,15 @@
 import express from 'express';
 import cors from 'cors';
-import { grabShowings } from './utils.js';
+import { Showing, grabShowings } from './utils.js';
 
+
+const MAX_FETCH_AGE = (60000 * 5); // 5 minutes in milliseconds
 
 if(!process.env.FRONTEND_URL || !process.env.EXPRESS_PORT) {
   console.error('One or more required environment variables are not set.\n' +
   'Required variables: FRONTEND_URL, EXPRESS_PORT\nExiting...');
   process.exit(1);
 }
-
-
 
 const app = express();
 app.use(cors());
@@ -23,14 +23,26 @@ app.use((_req, res, next) => {
 
 
 let isFetching = false; // Prevents multiple fetches at once.
+let lastFetch: Showing[] = []; // Cache the last fetch to prevent multiple fetches in a short time.
+let lastFetchTime = 0; // Time of the last fetch in milliseconds.
 
 app.get('/showtimes', async (_req, res) => {
 
   // Keep waiting until the last fetch is done
   while(isFetching) await new Promise(r => setTimeout(r, 100));
+
+  // If the cached fetch is younger than the max fetch age, return it.
+  if(lastFetchTime + MAX_FETCH_AGE > Date.now()) {
+    res.json(lastFetch);
+    return;
+  }
+
+
   isFetching = true;
 
   grabShowings().then(showings => {
+    lastFetch = showings;
+    lastFetchTime = Date.now();
     res.json(showings);
   }).catch(error => {
     console.error(error);
