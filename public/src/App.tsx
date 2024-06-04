cf58e6a1542ecfa3929a56b0680f516987df4529
Josh <joshuav2012@live.co.uk>
2024-06-05 00:28:06 +0100

    Basic API fetching

--- public/src/App.tsx
+++ public/src/App.tsx
@@ -1,25 +1,49 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  const [secsSinceUpdate, setSecsSinceUpdate] = useState(0);
+  const apiURL = process.env.REACT_APP_API_URL;
+
+  // Upon loading, keep ticking the time since last update:
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setSecsSinceUpdate(secsSinceUpdate + 1);
+    }, 1000);
+    return () => clearInterval(interval);
+  }, [secsSinceUpdate]);
+
+  // Fetch the schedule from the server:
+  useEffect(() => {
+    fetch(apiURL + '/showtimes')
+      .then(res => res.json())
+      .then(data => {
+        console.log(data);
+      });
+  }, [apiURL]);
+  
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h2>Schedule</h2>
+      <span className='flex-hoz'><h3>Last updated&nbsp;</h3><h3 className='bold'>{SecsToHMS(secsSinceUpdate)}</h3></span>
+      <span className='flex-hoz'><h3>Remaining&nbsp;</h3><h3 className='bold'>67</h3></span>
+
+      <div className='showing-list'>
+
+      </div>
     </div>
+  );
+}
+
+function SecsToHMS(secs: number): string {
+  const hours = Math.floor(secs / 3600);
+  const minutes = Math.floor((secs % 3600) / 60);
+  const seconds = Math.floor(secs % 60);
+  return (
+    (hours > 0 ? `${hours}h ` : '') +
+    (minutes > 0 ? `${minutes}m ` : '') +
+    (seconds > 0 ? `${seconds}s` : '')
   );
 }
 
